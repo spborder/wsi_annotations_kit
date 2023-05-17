d63126b4dbd4186a5b2f17e5cc561cbac20bda24
spborder <spborder@buffalo.edu>
2023-05-17 17:36:29 -0400

    create setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="demopackage",
+    version="1.0.0",
+    author="Sam Border",
+    author_email="sam.border2256@gmail.com",
+    description="Utility functions for generating, saving, and converting annotation files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spborder/wsi_annotations_kit",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
