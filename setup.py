6656d0a73fe298814c9e5ade67d96c8f21abc0b5
spborder <spborder@buffalo.edu>
2023-05-24 14:59:40 -0400

    setup debugging

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/spborder/wsi_annotations_kit",
+    install_requires=[
+        "lxml",
+        "geojson",
+        "shapely"
+    ],
     packages=setuptools.find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
