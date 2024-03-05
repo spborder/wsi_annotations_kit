99f2090a7ebb6ff99e88cf59b89f889266bef77e
spborder <spborder@buffalo.edu>
2024-03-05 17:18:45 -0500

    Adding comments in test script

--- examples/example_annotationpatches.py
+++ examples/example_annotationpatches.py
@@ -68,6 +68,7 @@ def main():
 
     # Now doing the same thing but using the AnnotationPatches method
     patch_annotation = wak.AnnotationPatches()
+    # Pre-define patches according to desired criteria
     patch_annotation.define_patches(
         region_crs = [0,0],
         height = np.shape(processed_mask)[0],
@@ -77,13 +78,16 @@ def main():
         overlap_pct = 0.25
     )
 
+    # Start patch iterator
     patch_annotation = iter(patch_annotation)
     keep_iterating = True
     while keep_iterating:
         try:
+            # Get region of a given patch using __next__()
             new_patch = next(patch_annotation)
             mask_region = processed_mask[new_patch.top:new_patch.bottom, new_patch.left:new_patch.right]
 
+            # Add specified region of the mask to total annotations
             patch_annotation.add_patch_mask(
                 mask = mask_region,
                 patch_obj = new_patch,
@@ -94,8 +98,10 @@ def main():
         except StopIteration:
             keep_iterating = False
 
+    # Clean up incomplete objects on the edges of patches
     patch_annotation.clean_patches()
-    # Testing saving annotations
+
+    # Save annotation to text file
     patch_annotation.xml_save('./examples/patch_test_xml.xml')
     patch_annotation.geojson_save('./examples/patch_test_geojson.geojson')
     patch_annotation.json_save('./examples/patch_test_json.json')
